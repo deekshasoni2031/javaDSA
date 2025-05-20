d14b2ef19be8d20d595c9e1bffae7b7aa4d08d44
deekshasoni2031 <deekshasoni2031@gmail.com>
2025-05-20 11:01:28 +0530

    Add files via upload

---
+++ CyclicRotation.java
@@ -0,0 +1,22 @@
+public class CyclicRotation {
+    static void rotation(int[] arr){
+        
+        int lv=arr[arr.length-1];
+        for (int i=arr.length-1;i>0;i--){
+            arr[i]=arr[i-1];
+        }
+        arr[0]=lv;
+
+        System.out.println("new array:");
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+        
+    }
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5};
+        rotation(arr);
+
+        }
+    }
+
